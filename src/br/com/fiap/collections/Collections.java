fc580fd4243dba793585b04800bb9d13230e4e80
GuiEspim18 <guilherme.monteiro.espim@gmail.com>
2023-11-28 23:08:34 -0300

    Collections

--- src/br/com/fiap/collections/Collections.java
+++ src/br/com/fiap/collections/Collections.java
@@ -1,26 +1,40 @@
 package br.com.fiap.collections;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Collections {
     public static void main (String[] args) {
-        List<String> shoppingCart = new ArrayList<String>();
-        System.out.println(shoppingCart.isEmpty()); // true
+//        List<String> shoppingCart = new ArrayList<String>();
+//        System.out.println(shoppingCart.isEmpty()); // true
+//
+//        shoppingCart.add("Maçã");
+//        shoppingCart.add("Pera");
+//        shoppingCart.add("Uva");
+//        shoppingCart.set(1, "Abacaxi");
+//
+//        System.out.println(shoppingCart.isEmpty()); // false
+//        System.out.println(shoppingCart.size()); // 3
+//
+//        String value = shoppingCart.get(2);
+//        System.out.println(value);
+//
+//        System.out.println(shoppingCart.contains("Maçã"));
+//        System.out.println(shoppingCart.indexOf("Maçã"));
+//        System.out.println(shoppingCart.indexOf("Jaca"));
 
-        shoppingCart.add("Maçã");
-        shoppingCart.add("Pera");
-        shoppingCart.add("Uva");
-        shoppingCart.set(1, "Abacaxi");
+        Map<String, String> box = new HashMap<String, String>();
+        box.put("GH1", "Guilherme");
+        box.put("GH2", "Heloísa");
 
-        System.out.println(shoppingCart.isEmpty()); // false
-        System.out.println(shoppingCart.size()); // 3
+        System.out.println(box.isEmpty());
+        System.out.println(box.size());
+        System.out.println(box.containsKey("GH1"));
+        System.out.println(box.containsValue("Heloísa"));
+        System.out.println(box);
+        System.out.println(box.get("GH1"));
 
-        String value = shoppingCart.get(2);
-        System.out.println(value);
-
-        System.out.println(shoppingCart.contains("Maçã"));
-        System.out.println(shoppingCart.indexOf("Maçã"));
-        System.out.println(shoppingCart.indexOf("Jaca"));
     }
 }
